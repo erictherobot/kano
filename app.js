273b517a4dd813ea8565ddd9eaac9fa4d038e8b2
Eric The Robot <erictherobot@gmail.com>
2014-04-07 19:20:58 -0400

    adding page title for 404 pages

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ app.use(function(req, res, next) {
 app.use(app.router);
 app.use(function(req, res) {
   res.status(404);
-  res.render('404');
+  res.render('404', {title: 'Error 404 - Page Not Found'});
 });
 app.locals.pretty = false;
 app.use(express.errorHandler());

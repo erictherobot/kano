e7b3e17b526e65017e5b0b72a9ac0335029ed9c7
erictherobot <erictherobot@gmail.com>
2015-08-26 11:51:23 -0400

    Replacing deprecated auto_reconnect with autoReconnect
    fixes connect-mongo deprecated auto_reconnect option is deprecated. Use
    autoReconnect instead

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ app.use(session({
   secret: secrets.sessionSecret,
   store: new MongoStore({
     url: secrets.db,
-    auto_reconnect: true
+    autoReconnect: true
   })
 }));
 app.use(passport.initialize());
